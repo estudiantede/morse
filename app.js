9087601c31d1322953527407daf53e1a658affb7
estudiantede <inakifrutos00@gmail.com>
2024-07-23 11:27:59 -0300

    Mover index

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/*
 app.get('/', (req, res) => {
       res.sendFile(__dirname + '/traductor/index.html')
+})
+      */
+
+app.use(express.static('traductor'))
+
+app.get('/', (req, res) => {
+    console.log("Se llamo al lugar /")
+    
 })
 
 app.listen(port, () => {
