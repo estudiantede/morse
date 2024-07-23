e5b4176ab840282c98d76fe743a074faf34d4090
estudiantede <inakifrutos00@gmail.com>
2024-07-23 11:32:07 -0300

    Arreglar app.js

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-/*
-app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/traductor/index.html')
-})
-      */
-
 app.use(express.static('traductor'))
 
 app.get('/', (req, res) => {
     console.log("Se llamo al lugar /")
-    
+    console.log(__dirname)
+    res.sendFile(__dirname + '/views/index.html')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
